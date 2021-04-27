ecc9ca88b205a0db416056a9f31429449ec01982
1007297 <1007297@1007297T1082753>
2021-04-27 10:49:21 +0430

    first commit

---
+++ API/Startup.cs
@@ -0,0 +1,59 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+using MediatR;
+using Application.Activities;
+
+namespace API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<DataContext>(optionsAction=>{
+                optionsAction.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
+            });
+            services.AddCors(opt=>{
+                opt.AddPolicy("CorsPolicy",policy=>{
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
+            });
+            services.AddMediatR(typeof(List.Handler).Assembly);
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseCors("CorsPolicy");
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
---
+++ client-app/src/App.tsx
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import "./App.css";
+import axios from "axios";
+import { Header, Icon, List } from "semantic-ui-react";
+
+class App extends Component {
+  state = {
+    values: [],
+  };
+
+  componentDidMount() {
+    axios.get("http://localhost:5000/api/activities").then((response) => {
+      this.setState({
+        values: response.data,
+      }); 
+      
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Header as="h2">
+          <Icon name="users" />
+          <Header.Content>مدیریت محصولات سایپا</Header.Content>
+        </Header>
+
+        <List>
+          {this.state.values.map((value: any) => (
+            <List.Item key={value.id}>{value.title}</List.Item>
+          ))}
+        </List>
+
+      </div>
+    );
+  }
+}
+
+export default App;
