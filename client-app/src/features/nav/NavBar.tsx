b4af5a5663d1b7dd60ce2109bf38b1455fb9883a
1007297 <1007297@1007297T1082753>
2021-05-03 16:02:09 +0430

    3333333

--- client-app/src/features/nav/NavBar.tsx
+++ client-app/src/features/nav/NavBar.tsx
@@ -1,17 +1,21 @@
 import React from "react";
 import { Button, Container, Menu } from "semantic-ui-react";
 
-const NavBar = () => {
+interface IProps{
+    openCreateForm:()=>void;
+}
+
+const NavBar:React.FC<IProps> = ({openCreateForm}) => {
   return (
     <Menu fixed="top" inverted>
       <Container>
         <Menu.Item header>
-          <img src="/assets/logo.png" alt="logo" />
+          <img src="/assets/logo.png" alt="logo" style={{marginRight:'10px',width:'70px'}} width="100" />
           مدیریت محصولات سایپا
         </Menu.Item>
         <Menu.Item name="Activities" />
         <Menu.Item >
-            <Button positive content='ایجاد محصول جدید'/>
+            <Button onClick={openCreateForm} positive content='ایجاد محصول جدید'/>
         </Menu.Item>
       </Container>
     </Menu>
